293772681903994b494c74d9fc879c8d9d5713ca
kleenkanteen <antpalmolive@gmail.com>
2023-08-23 12:39:06 -0400

    createRoom get and post endpoint done

--- site/src/app/api/room/route.ts
+++ site/src/app/api/room/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST(req: Response) {
   try {
     const new_room = await prisma.rooms.create({
         data: {
@@ -9,15 +9,11 @@ export async function POST(req: Request) {
         }
     })
 
-    if (!new_room) {
-      throw new Error("Error creating room");
-    }
-    
     return NextResponse.json({
         room_id: new_room.room_id
     });
   } catch (error) {
-    console.log(error);
-    return NextResponse.error();
+    console.error(`Error creating room: `, error);
+    return new NextResponse("Error creating room", { status: 500 });
   }
 }
