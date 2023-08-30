a8ca7ba55e36f4640e7d341bc4699976c0e42f43
LucileTech <lucile.t.tech@gmail.com>
2023-08-30 09:18:23 -0700

    discard changes from main merge

--- site/src/app/api/room/createRoom.ts
+++ site/src/app/api/room/createRoom.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { getBaseUrl } from "@/lib/server/getBaseUrl";
 import { scheduleRoomDeletion } from "./deleteRoom"; // Import the scheduled deletion function
 import prisma from "@/lib/prisma"; // Import your Prisma client instance
@@ -30,21 +29,4 @@ export async function createRoom() {
   scheduleRoomDeletion(room.room_id, 24 * 60 * 60 * 1000); // 24 hours in milliseconds
 
   return room.room_id;
-=======
-import { getBaseUrl } from '@/lib/server/getBaseUrl';
-
-export async function createRoom() {
-	const baseUrl = getBaseUrl();
-	const res = await fetch(`${baseUrl}/api/room`, {
-		method: 'POST',
-	});
-
-	// On failed request, return null middleware for redirect
-	if (res.status == 500) {
-		return null;
-	}
-
-	const room = await res.json();
-	return room.room_id;
->>>>>>> main
 }
--- site/src/lib/server/getBaseUrl.ts
+++ site/src/lib/server/getBaseUrl.ts
@@ -1,11 +1,5 @@
 export function getBaseUrl() {
-<<<<<<< HEAD
   // Serverside function to get the base url & port
   const baseUrl = "http://localhost:3000";
   return baseUrl;
-=======
-	// Serverside function to get the base url & port
-	const baseUrl = 'http://localhost:3000';
-	return baseUrl;
->>>>>>> main
 }
