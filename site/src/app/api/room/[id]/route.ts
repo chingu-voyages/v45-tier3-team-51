1432d02afc70ac6483f78d56caf3ee5071e99083
kennytram <kennyvtram@gmail.com>
2023-09-06 04:30:47 -0700

    add the delete api route for testing purposes

--- site/src/app/api/room/[id]/route.ts
+++ site/src/app/api/room/[id]/route.ts
@@ -21,3 +21,20 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
     return new NextResponse("Error fetching room", { status: 500 });
   }
 }
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+  const id = params.id;
+  
+  try {
+    const room = await prisma.rooms.delete({
+      where: {
+        room_id: parseInt(id),
+      },
+    });
+    
+    return NextResponse.json("Deleted a room");
+  } catch (error) {
+    console.error(`Error fetching room with ID ${id}: `, error);
+    return new NextResponse("Error deleting a  room", { status: 500 });
+  }
+}
--- site/tests/api.spec.ts
+++ site/tests/api.spec.ts
@@ -2,22 +2,34 @@ import { test, expect } from '@playwright/test';
 
 const apiUrl = 'http://localhost:3000/api';
 
-test('should create a room', async ({ request }) => {
-    const response = await request.post(`${apiUrl}/room`);
-    await expect(response).toBeOK();
+test.describe.serial('Generate a new room', () => {
+    let roomId: number;
+
+    test('should create a room', async ({ request }) => {
+        const response = await request.post(`${apiUrl}/room`);
+        const responseBody = await response.json();
+        await expect(response).toBeOK();
+        roomId = responseBody.room_id;
+    });
+
+    test('should get a room', async ({ request }) => {
+        const response = await request.get(`${apiUrl}/room/${roomId}`);
+        await expect(response).toBeOK();
+    });
+
+    test('should delete a room', async ({ request }) => {
+        const response = await request.delete(`${apiUrl}/room/${roomId}`);
+        console.log(response.status());
+        await expect(response).toBeOK();
+    });
+
+    test('should fail getting a room', async ({ request }) => {
+        const response = await request.get(`${apiUrl}/room/${roomId}`);
+        expect(response.status()).toBe(404);
+    });
 });
 
-test('should get a room', async ({ request }) => {
-    const response = await request.get(`${apiUrl}/room/2`);
-    await expect(response).toBeOK();
-});
 
-test('should delete a room', async ({ request }) => {
-    const response = await request.delete(`${apiUrl}/room/2`);
-    await expect(response).toBeOK();
-});
 
-test('should fail getting a room', async ({ request }) => {
-    const response = await request.get(`${apiUrl}/room/NotFound`);
-    expect(response.status()).toBe(500);
-});
+
+
