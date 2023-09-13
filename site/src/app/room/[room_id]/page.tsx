d5b4a2a4c1088729fb8f7b95cbb40d168555cf13
gamadjg <gamadavid36@gmail.com>
2023-09-12 21:56:41 -0700

    added conditionals to support same page completion page

--- site/src/app/room/[room_id]/page.tsx
+++ site/src/app/room/[room_id]/page.tsx
@@ -5,12 +5,13 @@ import { Footer } from '@/components/Footer';
 import { Buttons } from '@/components/Buttons';
 import { CopyLink } from '@/components/CopyLink';
 import { Display } from '@/components/Display';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { useEffect, useMemo, useState } from 'react';
 import { pusherClient } from '@/lib/pusher';
 import { getBaseUrl } from '@/lib/server/getBaseUrl';
 
 export default function Home() {
+	const router = useRouter();
 	const path = usePathname();
 	const roomId = path.split('/')[2];
 	const [questionText, setQuestionText] = useState('');
@@ -64,12 +65,30 @@ export default function Home() {
 			method: 'PATCH',
 		});
 	};
+
+	// const completionPage = () => {
+	// 	router.push();
+	// };
+
 	return (
 		<main className='flex flex-col min-h-screen p-4'>
 			<HeaderAction />
 			<div className='flex-grow flex flex-col items-center justify-center'>
-				<div className='mb-6 text-center'>{isLoading ? <></> : <Display text={questionText} />}</div>
+				<div className='mb-6 text-center'>
+					{isLoading ? (
+						// if loading don't display anything
+						<></>
+					) : currentQuestion < 20 ? (
+						// if currentQuesion is below 20 (hardcoded) display the quesiton text
+						<Display text={questionText} />
+					) : (
+						// if we're passed Q20, display completion, no route as that requires another trigger
+						<Display text={'Thanks for playing!'} />
+					)}
+				</div>
+
 				{currentQuestion == 0 ? (
+					// if currentQuestion is 0, room has not started, display share link, directions, and Start game button
 					<div className='flex flex-col gap-3'>
 						<Display text={'Share the link below with other participants'} />
 						<div className='mb-6 text-center'>
@@ -78,9 +97,14 @@ export default function Home() {
 						<Buttons text='Start Game' size='lg' onClick={nextQuestionText} />
 					</div>
 				) : isLoading ? (
+					// room questions have begun, however the page is waiting for the trigger to provide the next question text. display loading for now.
 					<div>Loading...</div>
-				) : (
+				) : currentQuestion < 20 ? (
+					// room has started and has not finished, display Next Question on button and use onClick nextQuestionText.
 					<Buttons text='Next question' size='lg' onClick={nextQuestionText} />
+				) : (
+					// if room has reached max number of questions, don't display anything.
+					<></>
 				)}
 				{/* <div className='mt-6 text-center'> */}
 				{/* {currentQuestion.includes('Waiting for') ? (
