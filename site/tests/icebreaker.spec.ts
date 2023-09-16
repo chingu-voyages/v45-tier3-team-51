ece9843505ff1708825253565bbadf461a2be447
kennytram <kennyvtram@gmail.com>
2023-09-16 00:40:08 -0700

    add more test cases

--- site/tests/icebreaker.spec.ts
+++ site/tests/icebreaker.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
+import { getBaseUrl } from '../src/lib/server/getBaseUrl';
 
-// const url = 'https://chingu.sabih.me/';
-const url = 'http://localhost:3000/';
+const url = getBaseUrl();
 
 const titleContent = 'Break the ice with fun questions!';
 
@@ -39,10 +39,11 @@ test('create room button redirects you to the room page', async ({ page }) => {
 	await page.goto(url);
 
 	const button = page.getByRole('button', { name: 'Create Room' });
+	const regexRoomUrl = new RegExp(`${url}/room/[0-9]+`);
 
 	button.click();
 
-	const regexRoomUrl = new RegExp(`${url}room/[0-9]+`);
+	await page.waitForURL(regexRoomUrl);
 
 	await expect(page).toHaveURL(regexRoomUrl);
 });
@@ -51,10 +52,11 @@ test('room page contains a shareable room url', async ({ page }) => {
 	await page.goto(url);
 
 	const button = page.getByRole('button', { name: 'Create Room' });
+	const regexRoomUrl = new RegExp(`${url}/room/[0-9]+`);
 
 	button.click();
 
-	const regexRoomUrl = new RegExp(`${url}room/[0-9]+`);
+	await page.waitForURL(regexRoomUrl);
 
 	const shareableUrl = page.getByText(regexRoomUrl);
 
@@ -72,4 +74,103 @@ test('room page contains a shareable room url', async ({ page }) => {
 	});
 
 	expect(clipboardText).toEqual(page.url());
+});
+
+test('game works properly', async ({ page }) => {
+	page.goto(url);
+
+	const createButton = page.getByRole('button', { name: 'Create Room' });
+	await createButton.click();
+
+	const regexRoomUrl = new RegExp(`${url}/room/[0-9]+`);
+
+	await page.waitForURL(regexRoomUrl);
+
+	const startButton = page.getByRole('button', { name: 'Start Game' });
+	await startButton.click();
+
+	const nxtButton = page.getByRole('button', { name: 'Next question' });
+	await nxtButton.waitFor({ timeout: 5000 });
+
+	let finishButtonFound = false;
+	while (!finishButtonFound) {
+		const finishButton = await page.$('button:has-text("finish")');
+
+		if (finishButton) {
+			finishButtonFound = true;
+		}
+		else {
+			const question = page.locator('h1.mantine-Text-root');
+			await expect(question).toBeVisible();
+
+			const nextButton = page.getByRole('button', { name: 'Next question' });
+			await expect(nextButton).toBeVisible();
+			await expect(nextButton).toBeEnabled();
+			await nextButton.click();
+			await page.waitForTimeout(3000);
+		}
+	}
+	const finishButton = page.getByRole('button', { name: 'finish' });
+	await finishButton.click();
+
+	await page.waitForTimeout(3000);
+
+	const finishText = page.locator('h1.mantine-Text-root');
+	await expect(finishText).toBeVisible();
+	await expect(finishText).toHaveText('Thanks for playing!');
+});
+
+test('pusher works properly', async ({ browser, context }) => {
+	const firstPage = await context.newPage();
+	await firstPage.goto(url);
+
+	const button = firstPage.getByRole('button', { name: 'Create Room' });
+	await button.click();
+
+	const regexRoomUrl = new RegExp(`${url}/room/[0-9]+`);
+	await firstPage.waitForURL(regexRoomUrl);
+
+	const copyButton = firstPage.getByRole('button', { name: 'Copy' });
+
+	await copyButton.click();
+
+	const clipboardText = await firstPage.evaluate(async () => {
+		return navigator.clipboard.readText();
+	});
+
+	const secondPage = await context.newPage();
+	await secondPage.goto(clipboardText);
+
+	const startButton = firstPage.getByRole('button', { name: 'Start Game' });
+	await startButton.click();
+
+	const nxtButton = firstPage.getByRole('button', { name: 'Next question' });
+	await nxtButton.waitFor({ timeout: 5000 });
+
+	let finishButtonFound = false;
+	while (!finishButtonFound) {
+		const finishButton = await firstPage.$('button:has-text("finish")');
+
+		if (finishButton) {
+			finishButtonFound = true;
+		}
+		else {
+			const question1 = await firstPage.locator('h1.mantine-Text-root').textContent();
+			const question2 = await secondPage.locator('h1.mantine-Text-root').textContent();
+			expect(question1 === question2).toBe(true);
+
+			const nextButton = firstPage.getByRole('button', { name: 'Next question' });
+			await nextButton.click();
+			await firstPage.waitForTimeout(3000);
+		}
+	}
+	const finishButton = firstPage.getByRole('button', { name: 'finish' });
+	await finishButton.click();
+
+	await firstPage.waitForTimeout(3000);
+
+	const finishText1 = await firstPage.locator('h1.mantine-Text-root').textContent();
+	const finishText2 = await secondPage.locator('h1.mantine-Text-root').textContent();
+	expect(finishText1 === finishText2).toBe(true);
+
 });
